070a4b508cabdab7eba4bedb245eadea203731c6
Damian Kądziela <dkkquality@gmail.com>
2022-09-02 18:04:32 +0200

    rates slice added

---
+++ src/features/ratesSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = []
+
+const ratesSlice  = createSlice({
+    name: "rates",
+    initialState,
+    reducers: {
+        addRate: (state, action) => {
+            state = [...state, action.payload]
+        },
+        editRate: (state, action) => {
+            state = state.map(rate => {
+                if(rate.id === action.payload.id){
+                    return action.payload
+                } else {
+                    return rate
+                }
+            })
+        },
+        removeRate: (state) => {
+            state = state.filter(state => state.id !== action.payload.id)
+        }
+    }
+})
+
+export const { addRate, editRate, removeRate } = ratesSlice.actions
+
+export default ratesSlice.reducer
